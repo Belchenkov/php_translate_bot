735009802dd4e2856abcc1c50796db808b01a905
belchenkov <u608110@gmail.com>
2023-06-13 10:24:34 +0300

    Output keyboard with languages

--- index.php
+++ index.php
@@ -18,3 +18,27 @@ file_put_contents(__DIR__ . '/logs.txt', print_r($update, 1), FILE_APPEND);
 $chat_id = $update['message']['chat']['id'];
 $text = $update['message']['text'];
 
+if ($text == '/start') {
+    $data = get_chat_id($chat_id);
+
+    if (empty($data)) {
+        add_chat_id($chat_id, $update['message']['chat']['first_name'], 'en');
+        $check = 'en';
+    } else {
+        $check = $data['lang'];
+    }
+
+    $response = $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => "Оставьте отмеченный язык для перевода с него или выберите другой",
+        'reply_markup' => $telegram->replyKeyboardMarkup([
+            'inline_keyboard' => [
+                [
+                    ['text' => 'en', 'callback_data' => 'en'],
+                    ['text' => 'ru', 'callback_data' => 'ru'],
+                ]
+            ]
+        ])
+    ]);
+}
+
